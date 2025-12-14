90c6ec9e62bad5948f11378c25d2773454916c6d
unknown <ifjhfhkl@gmail.com>
2025-12-14 11:22:32 +0900

    date 2025/12/14

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@ npm run build
 
 # xserver にデプロイ
 scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 dist/* hasunecocoro@sv16731.xserver.jp:~/hasunecocoro.xsrv.jp/public_html/personal/astro
-scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 public hasunecocoro@sv16731.xserver.jp:~/hasunecocoro.xsrv.jp/public_html/personal/astro
+scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 public/* hasunecocoro@sv16731.xserver.jp:~/hasunecocoro.xsrv.jp/public_html/personal/astro
 
 # github にデプロイ
 $date = Get-Date -Format "d" 
