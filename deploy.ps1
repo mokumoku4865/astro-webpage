ce77906a204b4b3130c2049234391b75c0ba21c2
unknown <ifjhfhkl@gmail.com>
2025-12-14 11:03:16 +0900

    date 2025/12/14

---
+++ deploy.ps1
@@ -0,0 +1,15 @@
+# ビルドする
+cd F:\Web\astro\blue-belt
+npm run build
+
+# xserver にデプロイ
+scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 dist/* hasunecocoro@sv16731.xserver.jp:~/hasunecocoro.xsrv.jp/public_html/personal/astro
+scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 public hasunecocoro@sv16731.xserver.jp:~/hasunecocoro.xsrv.jp/public_html/personal/astro
+
+# github にデプロイ
+$date = Get-Date -Format "d" 
+$stamp="date " +$date
+git add .
+git commit -m $stamp
+git branch -M main
+git push -u origin main
