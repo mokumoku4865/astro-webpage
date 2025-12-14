5d1ef02b62f65b0d27b952b10b8a8f3fddaf8f2f
unknown <ifjhfhkl@gmail.com>
2025-12-14 14:57:40 +0900

    date 2025/12/14

--- deploy.ps1
+++ deploy.ps1
@@ -3,8 +3,8 @@ cd F:\Web\astro\blue-belt
 npm run build
 
 # xserver にデプロイ
-scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 dist/* hasunecocoro@sv16731.xserver.jp:~/mokuren7132.com/public_html/astro
-scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 public/* hhasunecocoro@sv16731.xserver.jp:~/mokuren7132.com/public_html/astro
+scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 dist/* hasunecocoro@sv16731.xserver.jp:~/mokuren7132.com/public_html/web.mokuren7132.com/astro
+scp -r -i C:\Users\ifjhf\.ssh\hasunecocoro.key -P 10022 public/* hhasunecocoro@sv16731.xserver.jp:~/mokuren7132.com/public_html/web.mokuren7132.com/astro
 
 # github にデプロイ
 $date = Get-Date -Format "d" 
